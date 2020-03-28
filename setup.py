28aae0835e50719971c365d08213a78de887b4bd
prince roshan <princekrroshan01@gmail.com>
2020-03-29 00:46:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description=("A  CLI tool for youtube data api v3"),
     long_description=read('README.md'),
     license="MIT",
-    py_modules=['cli'],
+    py_modules=['helper','cli'],
     entry_points={'console_scripts': ['humdrum = cli:main']},
     install_requires=['google-api-python-client', 'click'],
     include_package_data=True
