ad897169c763833226d378cce4528cc75a0b6d02
prince roshan <princekrroshan01@gmail.com>
2020-04-13 10:20:44 +0530

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,49 +1,51 @@
 import click
-import webbrowser
 from apiclient.discovery import build
-from helper import API
 
+from helper import Api
 
 
 @click.command()
-@click.option('--count', default=5, help = 'enter the number of video you are looking for.')
-@click.option('--title', prompt='title of the video', help = 'enter the title name of video you are looking for')     
-@click.option('--order', help = 'to sort the video by date rating viewCount') 
-@click.option('--type', help = 'to search for type of video like channel video playlist')
+@click.option('--count', default=5, help='enter the number of video you are looking for.')
+@click.option('--title', prompt='title of the video',
+              help='enter the title name of video you are looking for')
+@click.option('--order', help='to sort the video by date rating viewCount')
+@click.option('--type', help='to search for type of video like channel video playlist')
 def titles(count, title, order, type):
-	'''
-	obj is the instance of the API class having function
-	1 open_id
-	2 get_titles
-	3 get_description
-	and more	
-	'''
-	obj = API(title, count, order, type)
-	titles = obj.get_titles()
-	for j, i in enumerate(titles):
-		click.echo('{0} > title {1}'.format(j,i))
-	vid = value = click.prompt('type id for more', type=int)
-	open_link(obj, vid)
+    """
+    Function which acts as an interface with API wrapper and CLI
+    obj is the instance of the API class having function
+    1 open_id
+    2 get_titles
+    3 get_description
+    and more
+    """
+
+    obj = Api(title, count, order, type)
+    titles = obj.get_titles()
+    for j, i in enumerate(titles):
+        click.echo('{0} > title {1}'.format(j, i))
+    vid = click.prompt('type id for more', type=int)
+    open_link(obj, vid)
 
 
-def ffcall(i,obj,vid):
-	'''
-	helper function to choose between 2 function to call i.e. get_description and open_id
-	'''
-	if(i==0):
-		l=obj.get_descriptions() # function to get description of the video
-		print(l[vid])
-	if(i==1):
-		obj.open_id(vid) # function to open the video through default browser of the system
-		
+def ffcall(option, obj, vid):
+    """
+    helper function to choose between 2 function to call i.e. get_description and open_id
+    """
 
-def open_link(obj,vid):
-	while(1):
-		print("0. get video description\n1. open video url")
-		l=int(input())
-		ffcall(l, obj, vid)
-		
+    if option == 0:
+        des = obj.get_descriptions()  # function to get description of the video
+        print(des[vid])
+    if option == 1:
+        obj.open_id(vid)  # function to open the video through default browser of the system
 
-	
+
+def open_link(obj, vid):
+    while True:
+        print("0. get video description\n1. open video url")
+        option = int(input())
+        ffcall(option, obj, vid)
+
+
 if __name__ == '__main__':
     titles()
