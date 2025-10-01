95d4491cefb66f415d61cbf2702c4f213e459809
tulu-g559 <garnab559@gmail.com>
2025-10-01 20:16:00 +0530

    Reverted to old helper.py

--- helper.py
+++ helper.py
@@ -30,77 +30,49 @@ class Api:
         self.order = order
         self.type = type
 
-        # STEP 1: Get search results (basic snippet data)
-        search_req = self.youtube.search().list(
+        req = youtube.search().list(
             q=self.keyword,
             part="snippet",
             maxResults=self.maxlen,
             type=self.type,
             order=self.order,
         )
-        search_response = search_req.execute()
-
-        # STEP 2: Extract video IDs
-        video_ids = [
-            item["id"]["videoId"]
-            for item in search_response["items"]
-            if "videoId" in item["id"]
-        ]
-
-        # STEP 3: Get detailed video info (snippet + stats + contentDetails)
-        if video_ids:
-            details_req = self.youtube.videos().list(
-                part="snippet,statistics,contentDetails",
-                id=",".join(video_ids),
-            )
-            details_response = details_req.execute()
-            self.result = details_response
-        else:
-            self.result = {"items": []}
+        self.result = req.execute()
 
     def open_id(self, item_no):
-        """Opens the video in default browser of the system."""
+        """"Opens the video in default browser of the system."""
         return webbrowser.open(
             "https://www.youtube.com/watch?v="
-            + self.result["items"][item_no]["id"]
-        )
+            + self.result["items"][item_no]["id"]["videoId"]
+            )
 
     def get_titles(self):
-        """Returns a list with titles of the videos."""
-        return [item["snippet"]["title"] for item in self.result["items"]]
+        """"Returns a list with title of the videos."""
+        rslt = []
+
+        for i in range(self.maxlen):
+            rslt.append(self.result["items"][i]["snippet"]["title"])
+
+        return rslt
 
     def get_descriptions(self):
-        """Return list with descriptions of the videos."""
-        return [item["snippet"]["description"] for item in self.result["items"]]
+        """"Return list with description of the video."""
+        rslt = []
+
+        for i in range(self.maxlen):
+            rslt.append(self.result["items"][i]["snippet"]["description"])
+
+        return rslt
 
     def get_image_urls(self):
-        """Returns a list of thumbnail image URLs."""
-        return [item["snippet"]["thumbnails"]["medium"]["url"] for item in self.result["items"]]
+        """
+        Returns a list of image urls.
+        """
+        rslt = []
 
-    # NEW: Get channel names
-    def get_channels(self):
-        """Returns list of channel titles (uploaders)."""
-        return [item["snippet"]["channelTitle"] for item in self.result["items"]]
+        for _ in range(self.maxlen):
+            rslt.append(
+                self.result["items"][0]["snippet"]["thumbnails"]["medium"]["url"]
+            )
 
-    # NEW: Get publish dates
-    def get_publish_dates(self):
-        """Returns list of publishedAt dates."""
-        return [item["snippet"]["publishedAt"] for item in self.result["items"]]
-
-    # NEW: Get video statistics (views, likes, comments)
-    def get_stats(self):
-        """Returns list of dicts with video stats."""
-        stats_list = []
-        for item in self.result["items"]:
-            stats = item.get("statistics", {})
-            stats_list.append({
-                "views": stats.get("viewCount"),
-                "likes": stats.get("likeCount"),
-                "comments": stats.get("commentCount"),
-            })
-        return stats_list
-
-    # NEW: Get video durations
-    def get_durations(self):
-        """Returns list of video durations (ISO 8601 format)."""
-        return [item["contentDetails"]["duration"] for item in self.result["items"]]
+        return rslt
